f5ec558d2b001fbd4809aadcee176a3125628061
SoYeong Choi <thdud3410@gmail.com>
2021-02-13 01:37:53 +0900

    스프링 입문

---
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,46 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.*;
+
+/** 그냥 JPA 를 썼을 때 CONFIG
+@Configuration
+public class SpringConfig {
+    private final DataSource dataSource;
+    private final EntityManager em;
+
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JpaMemberRepository(em);
+    }
+}
+*/
+/** SPRING JPA 를 썼을 때 SPRING CONFIG */
+@Configuration
+public class SpringConfig {
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+}
